dd34221974249e5cc46eafec6f14f255d6657edb
Chris Bednarczyk <chris@tourradar.com>
2020-04-09 10:13:13 +0200

    Initial commit

---
+++ getText.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+xgettext --keyword=_ --language=PHP --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.pot ./src/*
---
+++ src/SampleClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+/**
+ * Class SampleClass
+ * @package App
+ */
+class SampleClass
+{
+    public function helloWorld() {
+        echo _("Hello World");
+    }
+}
