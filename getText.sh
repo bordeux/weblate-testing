26b23fec49f9bfc1bec7a818149f187cd1257d00
Chris Bednarczyk <chris@tourradar.com>
2020-04-09 13:24:41 +0200

    Initial commit

--- getText.sh
+++ getText.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 xgettext --keyword=_ --language=PHP --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.pot ./src/*
-msgmerge --update ./locale/en/LC_MESSAGES/messages.po ./locale/en/LC_MESSAGES/messages.pot
+msgmerge --previous --update ./locale/en/LC_MESSAGES/messages.po ./locale/en/LC_MESSAGES/messages.pot
