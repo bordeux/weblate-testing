55688e2136f09b6bea82a8c75c9f497873153013
Chris Bednarczyk <chris@tourradar.com>
2020-04-09 11:47:58 +0200

    Initial commit

--- getText.sh
+++ getText.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-xgettext --keyword=_ --language=PHP --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.pot ./src/*
+xgettext --keyword=_ --language=PHP --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.po ./src/*
