f05a7f1f8c140e6851748ad653491bf43caa63b0
Chris Bednarczyk <chris@tourradar.com>
2020-04-15 13:42:19 +0200

    POC of seperate files per domain

--- getText.sh
+++ getText.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-xgettext --keyword=_ --language=PHP --keyword=translate:1,2c --keyword=translate --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.pot ./src/*
-msgmerge --previous --update ./locale/en/LC_MESSAGES/messages.po ./locale/en/LC_MESSAGES/messages.pot
+xgettext --keyword=_ --from-code=UTF-8 --language=PHP --keyword=translate:1,2c --keyword=translate --sort-output --add-comments -o ./locale/en/LC_MESSAGES/messages.pot ./src/*
+msginit --no-translator --input=./locale/en/LC_MESSAGES/messages.pot -o ./locale/en/LC_MESSAGES/messages.pot --locale=en.UTF-8
+
+
+DOMAINS=('homepage' 'sample')
+
+for domain in "${DOMAINS[@]}"; do
+  msggrep -J -e homepage -o - ./locale/en/LC_MESSAGES/messages.pot | sed '/^msgctxt/d' > ./locale/en/LC_MESSAGES/${domain}.pot
+  #touch ./locale/en/LC_MESSAGES/${domain}.po
+  msgmerge --previous --update ./locale/en/LC_MESSAGES/${domain}.po ./locale/en/LC_MESSAGES/${domain}.pot
+  rm ./locale/en/LC_MESSAGES/${domain}.pot
+done
+
+rm ./locale/en/LC_MESSAGES/messages.pot
+
